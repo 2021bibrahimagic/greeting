55c85526990fdcfa8aaabcb060caf1d090d42db1
2021bibrahimagic <2021bibrahimagic@dwight.edu>
2019-09-17 13:16:03 +0000

    first

---
+++ greeting.py
@@ -0,0 +1,13 @@
+name1 = "Michaela"
+name2 = "Rebecca"
+age1= "15"
+age2= "16"
+
+
+print(name1)
+print(name2)
+print("hello," + name1)
+print(len(name2))
+print(name2 + " has " + str(len(name2)) + " characters in their name!")
+print(name1" is " +str(len(age1)) + " years old!")
+print(name1 + age1)
